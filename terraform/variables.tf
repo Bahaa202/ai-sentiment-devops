7c76b5af7782f69aeb817df4da3754140c2f322e
Bahaa Abdelkhalik <bebocode22@gmail.com>
2026-08-17 06:02:31 +0300

    feat: add Ansible podman role with SPAL repository support

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type (must stay in Free Tier)"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "key_pair_name" {
